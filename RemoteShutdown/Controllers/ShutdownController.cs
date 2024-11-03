b2aed72f69d9ac0d2930bf47ad335c4ef2d112ea
Szymon Domagała <75216504+goflyy@users.noreply.github.com>
2024-11-03 16:55:43 +0100

    Initial project setup with Visual Studio files

---
+++ RemoteShutdown/Controllers/ShutdownController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RemoteShutdown.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShutdownController : ControllerBase
+    {
+    }
+}
