71ea23f95fde797c7af5fc03da87be2db1bdce41
Szymon Domagała <75216504+goflyy@users.noreply.github.com>
2024-11-03 22:04:56 +0100

    Add RemoteShutdownApp project to solution for mobile functionality

--- RemoteShutdown/obj/Debug/net6.0/RemoteShutdownApi.AssemblyInfo.cs
+++ RemoteShutdown/obj/Debug/net6.0/RemoteShutdownApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RemoteShutdownApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3aea6097845f647da37350dbf121b62c9e322c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RemoteShutdownApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RemoteShutdownApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
